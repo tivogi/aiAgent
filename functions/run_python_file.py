30094657b5c0b929d0e9d6271fa644e5ade5b983
tomy <tomy@DESKTOP-G6HBVJG.localdomain>
2025-12-15 11:53:51 +0000

    Added run_python_file function

---
+++ functions/run_python_file.py
@@ -0,0 +1,23 @@
+import os
+import subprocess
+def run_python_file(working_directory, file_path, args=[]):
+    abs_working = os.path.abspath(working_directory)
+    abs_file = os.path.abspath(os.path.join(abs_working,file_path))
+    if not abs_file.startswith(abs_working):
+        return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
+    if not os.path.exists(abs_file):
+        return f'Error: File "{file_path}" not found.'
+    if not abs_file.endswith('.py'):
+        return f'Error: "{file_path}" is not a Python file.'
+    try:
+        completed_process = subprocess.run(["python", abs_file, *args],timeout=30,capture_output=True,text=True,cwd=abs_working)
+        if not completed_process.stdout and not completed_process.stderr:
+            return "No output produced"
+        output_str = f"STDOUT: {completed_process.stdout}\nSTDERR: {completed_process.stderr}"
+        if completed_process.returncode != 0:
+            output_str += f"Process exited with code {completed_process.returncode}"
+            return output_str
+        return output_str
+        
+    except Exception as e:
+        return f"Error: executing Python file: {e}"
